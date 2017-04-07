ac572b2ad4775f7b48536dd5ecbfa3105aa8842b
ebecheto <e.bechetoille@ipnl.in2p3.fr>
2017-04-07 12:33:34 +0200

    simple test pulser

---
+++ example/tb_DelayGeneratorGFT1004.py
@@ -0,0 +1,19 @@
+import DelayGeneratorGFT1004
+
+pul = DelayGeneratorGFT1004.DelayGeneratorGFT1004('192.168.0.43')
+
+delay=100#<== [ns] nanoseconde unit
+pul.send("DELAY T1,{}".format(delay))
+delay+=100
+pul.setDelay(1,delay)
+
+max=3000
+step=100
+nb=max/step
+
+for i in range(nb):
+    pul.setDelay(1,i*step)
+    raw_input("{}/{} => {}[ns] delay. next={}".format(i, nb, i*step,i*(step+1)))
+
+
+
