a0b4d791e0ee0e74b7b7f1fdaeead1adf319fdc9
e.bechetoille@ipnl.in2p3.fr <e.bechetoille@ipnl.in2p3.fr>
2022-01-05 14:53:29 -0200

    tektro mso first commit

--- drivers/MSO.py
+++ drivers/MSO.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 
 import socket,sys, errno
+import collections
 
 class MSO:
     BUFFER_SIZE = 32*1024
     MEAS=["MEAN", "MAX", "MIN", "STDD", "POPU"]
+    Measure = collections.namedtuple('Measure',
+        'SLOT TEXT AVG HIGH LAST LOW SIGMA SWEEPS')
     def __init__(self, ip, port=4000):
         self.name= "Agilent"
         self.ip=ip
@@ -38,12 +41,44 @@ class MSO:
             self.connect()
             self.send(MESSAGE)
 
+
+    def setMeasurement(self, slot, measurecommand):
+	"""
+            osc.setMeasureSlot(4, "MAX,C1" )# Lecroy
+            osc.setMeasureSlot(4, "AMPLITUDE,CH1" )# Tektro
+	"""
+	type, source=measurecommand.split(',')
+	self.send("MEASUREMENT:MEAS{0}:TYPE {1};MEASUREMENT:MEAS{0}:SOURCE {2}".format(slot, type, source))
+
+
+    def getMeasurement2(self, slot):
+	mes =  dict()
+	mes['TEXT']=''
+        mes['TEXT'] += self.send("MEASUREMENT:MEAS{0}:SOURCE?".format(str(slot)))
+        mes['TEXT'] += ","+ self.send("MEASUREMENT:MEAS{0}:TYPE?".format(str(slot)))
+	mes['SLOT']=slot
+	mes['SWEEPS']=self.send("MEASU:MEAS{0}:SUBGROUP:RESUlts:ALLAcqs:{1}? \"OUTPUT1\"".format(str(slot), "POPU"))
+	mes['AVG']=self.send("MEASU:MEAS{0}:SUBGROUP:RESUlts:ALLAcqs:{1}? \"OUTPUT1\"".format(str(slot), "MEAN"))
+	mes['HIGH']=self.send("MEASU:MEAS{0}:SUBGROUP:RESUlts:ALLAcqs:{1}? \"OUTPUT1\"".format(str(slot), "MAX"))
+	mes['LOW']=self.send("MEASU:MEAS{0}:SUBGROUP:RESUlts:ALLAcqs:{1}? \"OUTPUT1\"".format(str(slot), "MIN"))
+	mes['SIGMA']=self.send("MEASU:MEAS{0}:SUBGROUP:RESUlts:ALLAcqs:{1}? \"OUTPUT1\"".format(str(slot), "STDD"))
+	mes['LAST']=self.send("MEASU:MEAS{0}:RESUlts:CURRentacq:{1}?".format(str(slot), "MEAN"))
+	return MSO.Measure(**mes) #<== apply every elemet of the dictionnary to the tupple for name indexing
+#	return self.Measure(**mes)
+#	return mes
+
+
     def getMeasurement(self, slot):
+        """
+	self.send("MEASU:MEAS1:SUBGROUP:RESUlts:ALLAcqs:MEAN? \"OUTPUT1\"")
+	self.send("MEASU:MEAS1:SUBGROUP:RESUlts:ALLAcqs:POPU? \"OUTPUT1\"")
+        """
+        # while(self.send("MEASU:MEAS"+str(slot)+":SUBGROUP:RESUlts:ALLAcqs:POPU? \"OUTPUT1\"")=='0'):
+        #     print("DEBUG POPU")
+        #     True
         return [self.send("MEASU:MEAS"+str(slot)+":SUBGROUP:RESUlts:ALLAcqs:"+m+"? \"OUTPUT1\"") for m in self.MEAS]
 
-            
-        
-            
+
 
 
 #  " PULS:TRAN2:TRA"
