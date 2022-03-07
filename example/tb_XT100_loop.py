328702162f71b0c08389ba3f515ff5a3afb9a21f
ebecheto <e.bechetoille@ipnl.in2p3.fr>
2022-03-07 07:38:59 -0200

    param file with steps

--- example/tb_XT100_loop.py
+++ example/tb_XT100_loop.py
@@ -7,7 +7,7 @@ osc = MSO.MSO('169.254.222.26'); print( "Osc connected")
 osc.send("ACQuire:SEQuence:NUMSEQuence 200")
 osc.send("ACQUIRE:STOPAFTER SEQUENCE")
 
-# osc.send("DESE 1;*ESE 1;*SRE 32") #<= pas besoin, si utilisation correcte de *OPC?
+osc.send("DESE 1;*ESE 1;*SRE 32") #<= pas besoin, si utilisation correcte de *OPC?
 
 
 # #osc.send("CLEAR") # clear sweep pas besoin, Acquire ON RAZ
@@ -17,13 +17,32 @@ osc.send("ACQUIRE:STOPAFTER SEQUENCE")
 # sdev=osc.send("MEASU:MEAS3:RESUlts:ALLAcqs:STDD?")
 # pop=int(pop);print(pop, mean, sdev)
 
+#3.935000e-08
+pul.send("del 39.35 ns;*opc?")
+pul.send("REL?") # 0000000000111110
+pul.send("del 39.40 ns;*opc?")
+pul.send("REL?") # 0000000001101111
+pul.send("CTSTORE?") #=> '1' calibration table in use
+pul.send("CTSTORE? info")
+# 'DATE=2/1/2022_12:08:55_PM,SN=22031948,VNA=E5063A-MY54502577,MARKER_F=2777777777'
+# 0.625*62 = 38.75
+# 0.625*63 = 39.375
 
+
+start=39350#0
+step=0.5# ps
+stop=42000#<= 50ns max depth
 ## INIT
-fout=open("XT100/XT100_carac.dat", "w")
-pul.send("STEP 0.5 ps")
-pul.send("del 0;*opc?")
+fout=open("XT100_carac_"+str(start)+"_"+str(stop)+"_"+str(step)+".dat", "w")
+fout.write("# "+"\t".join(["i", "mean_dly", "sdev_dly", "riseTime", "riseTime_input"])+'\n')
+pul.send("STEP "+str(step)+" ps")
+pul.send("del "+str(start)+";*opc?")
+pul.send("del?")
 ## LOOP
-nb = int(50000 / 0.5)
+i=0
+nb = int((stop-start) / step)
+i=1
+
 for i in range(nb):
 #    print(i), 
     rien=pul.send("INC;*OPC?")
@@ -31,8 +50,10 @@ for i in range(nb):
     mean=osc.send("MEASU:MEAS3:RESUlts:ALLAcqs:MEAN?")
     sdev=osc.send("MEASU:MEAS3:RESUlts:ALLAcqs:STDD?")
     rel=int(pul.send("REL?")[8:],2) # relays
+    rt1050=osc.send("MEASU:MEAS4:RESUlts:ALLAcqs:MEAN?")
+    rtin=osc.send("MEASU:MEAS6:RESUlts:ALLAcqs:MEAN?")# RiseTime
     dly=pul.send("DEL?") # delays
-    datas=[str(i), mean, sdev, dly, rel]
+    datas=[str(i), mean, sdev, dly, str(rel), rt1050, rtin]
     msg="\t".join(datas)
     print(msg)
     rien=fout.write(msg+'\n')
